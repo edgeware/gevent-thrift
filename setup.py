855d412ce6081d38b14ff960ff18c5fc5a3182a4
Peter Renström <renstrom.peter@gmail.com>
2014-02-14 15:38:06 +0100

    Add install dependencies to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
+from setuptools import setup
 
-from distutils.core import setup
+kwargs = {
+    'name': 'gevent-thrift',
+    'version': '0.1',
+    'description': 'gevent bindings for Thrift',
+    'author': 'Johan Rydberg',
+    'author_email': 'johan.rydberg@gmail.com',
+    'url': 'https://github.com/edgeware/gevent-thrift',
+    'packages': ['gevent_thrift'],
+    'install_requires': [
+        'gevent==0.13.8',
+        'thrift'
+    ]
+}
 
-setup(name='gevent-thrift',
-      version='0.0',
-      description='gevent bindings for Thrift',
-      author='Johan Rydberg',
-      author_email='johan.rydberg@gmail.com',
-      url='https://github.com/edgeware/gevent-thrift',
-      packages=['gevent_thrift']
-     )
+setup(**kwargs)
