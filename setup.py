4ca2eba74720a7731e0bdee2d8751a7cf4087e9f
Peter Renström <peter.renstrom@edgeware.tv>
2014-04-10 17:17:54 +0200

    Don't use dict-to-kwargs expansion.

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python
 from setuptools import setup
 
-kwargs = {
-    'name': 'gevent-thrift',
-    'version': '0.1',
-    'description': 'gevent bindings for Thrift',
-    'author': 'Edgeware AB',
-    'author_email': 'info@edgeware.tv',
-    'url': 'https://github.com/edgeware/gevent-thrift',
-    'packages': ['gevent_thrift'],
-    'install_requires': [
-        'gevent==1.0',
-        'thrift==0.8.0'
-    ]
-}
-
-setup(**kwargs)
+setup(name='gevent-thrift',
+      version='0.1',
+      description='Gevent bindings for Thrift',
+      author='Edgeware AB',
+      author_email='info@edgeware.tv',
+      url='https://github.com/edgeware/gevent-thrift',
+      packages=['gevent_thrift'],
+      install_requires=[
+          'gevent==1.0',
+          'thrift==0.8.0'
+      ])
